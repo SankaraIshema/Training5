8aab5e9ab1a2a7f6f64b181922a2f877858145cc
Sankara <sankara.ishema@gmail.com>
2017-04-01 14:37:16 +0200

    CheckBox

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ForkJoinPool;
 
+import com.sankara.combo_box.Window;
 import com.sankara.fork_join.FolderScanner;
 import com.sankara.fork_join.ScanException;
 
@@ -11,34 +12,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Path theWay = Paths.get("C:/Users/User/Pictures");
-		String filter = "*.jpg";
-		FolderScanner fs = new FolderScanner(theWay, filter);
-		
-		try {
-			Long start = System.currentTimeMillis();
-			Long result = fs.sequentialScan();
-			Long end = System.currentTimeMillis();
-			System.out.println("\nThere are " + result + " files with the extension " + filter + ".");
-			System.out.println("Execution time is : " + (end - start) + " ms.");
-		}
-		catch(IOException e) { e.printStackTrace();}
-		catch(ScanException e) { e.printStackTrace();}	
-		
-		Path theWay2 = Paths.get("C:/Users/User/Pictures");
-		FolderScanner fs2 = new FolderScanner(theWay2, filter);
-		int processors = Runtime.getRuntime().availableProcessors();
-		ForkJoinPool pool = new ForkJoinPool(processors);
-		
-		Long start2 = System.currentTimeMillis();
-		
-		System.out.println("\n\n*************************************************************\n\n");
-		pool.invoke(fs2);
-		
-		Long end2   = System.currentTimeMillis();
-		
-		System.out.println("\nThere are " + fs.getResult() + " files with the extension " + filter + ".");
-		System.out.println("Execution time is : " + (end2 - start2) + " ms.");
+		com.sankara.check_box.Window window = new com.sankara.check_box.Window();
 	}
 }
 
